9b44ab4188a0888e61af00ded4bbf80ce0dbad9b
TobyRotimi <olurotimitobiloba2@gmail.com>
2026-06-18 13:12:21 +0100

    updated the icons change when in different modes

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const themeButton = document.getElementById("theme-toggle");
-
+const icon = document.querySelector("i")
 themeButton.addEventListener("click", function() {
   if (document.body.classList.contains("dark-mode")) {
     document.body.classList.remove("dark-mode");
     document.body.classList.add("light-mode");
-    console.log("You are in light mode");
+   icon.classList.remove("fa-moon");
+   icon.classList.add("fa-sun")
+
   } else {
     document.body.classList.remove("light-mode");
     document.body.classList.add("dark-mode");
-    console.log("Enjoy the darkness");
+   icon.classList.remove("fa-sun");
+   icon.classList.add("fa-moon");
+
   }        
-});         
+});        
