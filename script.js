fb6372496e4c16344502eae717f5c6a6a6b60992
TobyRotimi <olurotimitobiloba2@gmail.com>
2026-09-02 12:42:27 +0100

    made it to my personal porfolio

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-const themeButton = document.getElementById("theme-toggle");
-const icon = document.querySelector("i")
-themeButton.addEventListener("click", function() {
-  if (document.body.classList.contains("dark-mode")) {
-    document.body.classList.remove("dark-mode");
-    document.body.classList.add("light-mode");
-   icon.classList.remove("fa-moon");
-   icon.classList.add("fa-sun")
-
-  } else {
-    document.body.classList.remove("light-mode");
-    document.body.classList.add("dark-mode");
-   icon.classList.remove("fa-sun");
-   icon.classList.add("fa-moon");
-
-  }        
-});        
+console.log('Hello world!')
